9bc0deb3f45224a0938fd700ddd9100b11b62232
gazuzdoad <128269064+gazuzdoad@users.noreply.github.com>
2025-09-18 16:17:07 -0600

    Update FG.py

--- FG.py
+++ FG.py
@@ -1,3 +1,6 @@
 #Este programa da solución a ecuaciones de segundo grado por medio de la fórmula general.
 #Lectura de coeficientes
 print("Solución a la ecuación ax^2+bx+c=0")
+a=float(input("coeficiente a="))
+b=float(input("coeficiente b="))
+b=float(input("coeficiente c="))

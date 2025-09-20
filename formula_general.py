e9eff8e12dbeb5e7a3c75cf5859c08c27ba3a18b
gaznet79 <adrianhernadnez28@gmail.com>
2025-09-20 12:28:22 -0600

    Update formula_general.py
    Corregir errores en import, discriminante y name.

--- formula_general.py
+++ formula_general.py
@@ -1,14 +1,14 @@
 #Este programa da solución a ecuaciones de segundo grado por medio de la fórmula general.
 #Lectura de coeficientes
 #Importar bibliotecas
-import.math
+import math
 def formula_general(a,b,c):
   if a==0:
     return "No es una ecuación cuadrática"
   dis=b**2-4*a*c
   if dis<0:
     return "No tiene soluciones reales."
-  elif discriminante==0:
+  elif dis==0:
     x=-b/(2*a)
     return f"Una solución real: x={x}"
   else:
@@ -16,10 +16,11 @@ def formula_general(a,b,c):
     x2=(-b-math.sqrt(dis))/(2*a)
     return f"Dos soluciones reales: x1={x1}, x2={x2}"
     
-if _name_=="_main_":
+if __name__=="__main__":
   print("Solución a la ecuación ax^2+bx+c=0")
   a=float(input("coeficiente a="))
   b=float(input("coeficiente b="))
   c=float(input("coeficiente c="))
+
 resultado=formula_general(a,b,c)
 print(resultado)

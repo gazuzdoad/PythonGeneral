aacaa9d84d46bf788e81054fb494faaa738a12ab
gazuzdoad <128269064+gazuzdoad@users.noreply.github.com>
2025-09-20 11:48:03 -0600

    Update and rename FG.py to formula_general.py

--- FG.py
+++
@@ -1,7 +0,0 @@
-#Este programa da solución a ecuaciones de segundo grado por medio de la fórmula general.
-#Lectura de coeficientes
-print("Solución a la ecuación ax^2+bx+c=0")
-a=float(input("coeficiente a="))
-b=float(input("coeficiente b="))
-c=float(input("coeficiente c="))
-#Calculo del determinante

34d956a29606f21c495cb8e3b48288725ad37690
gaznet79 <adrianhernadnez28@gmail.com>
2025-09-27 14:40:25 -0600

    Update fgeneral.py

--- FG/fgeneral.py
+++ FG/fgeneral.py
@@ -1,1 +1,18 @@
 #Este programa cálcula la fórmula general
+import math
+a=float(input("Dame el valor de a: "))  
+b=float(input("Dame el valor de b: "))
+c=float(input("Dame el valor de c: "))  
+#Calculamos el discriminante de la ecuación
+dis=math.pow(b,2)-4*a*c
+if dis<0:
+    print("No hay solución real")
+elif dis==0:
+    x=-b/(2*a)
+    print(f"Hay una solución real:{x}")
+else:
+    x1=(-b+math.sqrt(dis))/(2*a)
+    x2=(-b-math.sqrt(dis))/(2*a)
+    print(f"Hay dos soluciones reales: {x1} y {x2}")    
+print("Fin del programa")
+    

05b852353d9ccd717ee57cd6998fa9bbe15b0613
gaznet79 <adrianhernadnez28@gmail.com>
2025-09-27 16:23:43 -0600

    Update fgeneral.py
    Mejora de la presentación

--- FG/fgeneral.py
+++ FG/fgeneral.py
@@ -19,7 +19,12 @@ e3=tk.Label(root, text="Valor de c:")
 e3.pack()
 c1=tk.Entry(root)
 c1.pack()   
-#Obtener los valores de a, b y c
+#Etiqueta y  para mostrar el resultado
+res=tk.Label(root, text="Resultado:")
+res.pack()
+
+#Función para calcular la solución
+
 def calcular_solucion():
     try:
         a = float(a1.get())
@@ -32,18 +37,33 @@ def calcular_solucion():
     #Discriminante
     dis=math.pow(b,2)-4*a*c
     if dis<0:
-        messagebox.showinfo("Resultado","No existe solución real")
+        resultado="Resultado","No existe solución real"
     elif dis==0:
         x=-b/(2*a)
-        messagebox.showinfo("Resultado","Solución única: x=%.2f" %x)
+        resultado="Resultado","Solución única: x=%.2f" %x
     else:
         x1=(-b+math.sqrt(dis))/(2*a)
         x2=(-b-math.sqrt(dis))/(2*a)
-        messagebox.showinfo("Resultado","Soluciones: x1=%.2f y x2=%.2f" %(x1,x2))
+        resultado="Resultado","Soluciones: x1=%.2f y x2=%.2f" %(x1,x2)
+    res.config(text=resultado)
+
+
 #Botón para calcular
 boton_calcular=tk.Button(root, text="Calcular", command=calcular_solucion)
 boton_calcular.pack()
 
 boton=tk.Button(root, text="Cerrar", command=root.destroy)
 boton.pack()
+#Posiccion de los elementos
+e1.place(x=50,y=50)
+e2.place(x=50,y=100)
+e3.place(x=50,y=150)
+a1.place(x=200,y=50)    
+b1.place(x=200,y=100)
+c1.place(x=200,y=150)
+res.place(x=50,y=300)
+boton_calcular.place(x=50,y=200)
+boton.place(x=50,y=250)    
+
+
 root.mainloop()
